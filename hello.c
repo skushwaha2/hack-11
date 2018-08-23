7c060a862fe7b8e232ae687def187365c89c3207
skushwaha2 <42126850+skushwaha2@users.noreply.github.com>
2018-08-23 11:16:34 -0500

    change of code to print my name and major

--- hello.c
+++ hello.c
@@ -1,16 +1,10 @@
-/**
- * Author: Chris Bourke
- * Date: 2016/11/02
- *
- * A simple hello world program in C
- *
- */
+
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Hello World!\n");
+  printf("saurav kushwaha computer science");
 
   return 0;
 }
